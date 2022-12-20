5ffc97c5edcbef8101f2a4ec0e9b50489e860088
Craig Abbott <admin@craigabbott.co.uk>
2022-12-20 11:44:34 +0000

    Updates

--- src/lib/modify/_tables.js
+++ src/lib/modify/_tables.js
@@ -13,8 +13,13 @@ async function modifyTables ($) {
   console.log('│  │  └─ Adding "No observations added" to blank <td>s')
   $('td').each(function () {
     const text = $(this).text().replace(/\s/g, '')
-    if (text.length === 0) $(this).html('<p>No obvservations added.</p>')
+    if (text.length === 0) $(this).html('<p>No observations added.</p>')
   })
 }
+
+// left 10% Success
+// left 10% Result
+// right 40% Obs
+// right 40% Action plan
 
 module.exports = modifyTables
